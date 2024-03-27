7f4757a5686fde8c74acff523667130e9d486fa3
harounc <coulibalyharouna19@gmail.com>
2024-03-27 16:04:09 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# main.py
+
+import operations
+
+# Demander l'opération jusqu'à ce que l'utilisateur en donne une qui est valide
+while True:
+  op = input("Entrez le l'opération à effectuer (+, -, *, /) : ")
+  if op in "+-*/": # il faut que ça appartient à ces caractères
+
+    # Mais n'oublie pas que si l'utilisateur tape "+*", la condition précédente serait vérifiée.
+    # Ce n'est donc pas suffisant
+    if len(op) == 1: # Il faut que ce soit un seul caractère
+      break
+  # Sinon, la boucle s'exécute encore
+
+a = int(input("Entrez l'opérande a : "))
+b = int(input("Entrez l'opérande b: "))
+
+resultat = None
+
+if op == "+":
+  resultat = operations.addition(a, b)
+
+elif op == "-":
+  resultat = operations.soustraction(a, b)
+
+elif op == "*":
+  resultat = operations.multiplication(a, b)
+
+elif op == "/":
+  resultat = operations.division(a, b)
+
+print(f"Le résultat de {a} {op} {b} est: {resultat} ")
---
+++ operations.py
@@ -0,0 +1,15 @@
+# operations.py
+
+def addition(a: int, b: int) -> int:
+  return a + b
+
+
+def soustraction(a: int, b: int) -> int:
+  return a - b
+
+def multiplication(a: int, b: int) -> int:
+  return a * b
+
+
+def division(a: int, b: int) -> float:
+  return a / b
